35af491aafa1955344669193b802878bb88e8c87
MaxTGV <maxtugovlife@gmail.com>
2021-01-21 23:56:06 +0600

    add sentry

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthProvider } from "./AuthContext";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://3d91c1872166438589752a58cc09fda0@o508904.ingest.sentry.io/5602333",
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 const client = new QueryClient();
 
